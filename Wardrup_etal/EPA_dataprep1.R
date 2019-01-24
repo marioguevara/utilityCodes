2991ecbe0bb72eccb304b8f670b65dca66c1c489
marioguevara <mguevara@udel.edu>
2019-01-24 12:42:07 -0500

    Update EPA_dataprep1.R

--- Wardrup_etal/EPA_dataprep1.R
+++ Wardrup_etal/EPA_dataprep1.R
@@ -16,14 +16,23 @@ dat$top[dat$top==1] <- 0
 
 for(i in 1:nrow(dat)){
 
-#
+#if not equal to 0
 
 if (dat$top[i]!=0){
 
+#top will be the value of bottom at the row i-1
+  
 dat$top[i] <- dat$bottom[i-1] 
 
 }}
 
+#verify values for the rows 21:40, explorre other rows
+
 (dat)[111:112][21:40,]
 
+#export as a csv file to verify in excel if needed
+
 write.csv(dat, file='fixedTopEPA_v1.csv')
+
+#import the coordinate site...
+
